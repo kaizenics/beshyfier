dd72c9c5b0edff8059a4ab9d643cd725f13e65a7
Niko <nicosejohnsoriano@gmail.com>
2023-07-07 18:40:15 +0800

    beshyfier-text-modifier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
+import autosize from 'autosize';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState();
+  const textareaRef = useRef();
+
+  useEffect(() => {
+    autosize(textareaRef.current);
+  }, []);
+
+  function handleInputChange(event) {
+    setInputText(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="home-body">
+       <div className="box-ctn"></div>
+        <div className="container-1">
+          <div className="text-header">
+            <p>Input text here</p>
+          </div>
+            <div className="box">
+             <textarea 
+             className="input-text" 
+             ref={textareaRef} 
+             value={inputText} 
+             onChange={handleInputChange}/>
+          </div>
+        </div>
+        <div className="container-2">
+          <div className="text-header">
+            <p>Beshified Text Result</p>
+            </div>
+            <div className="box-result">
+              <p className="input-result">{inputText}</p>
+            </div>
+          </div>
+      </section>
     </div>
   );
 }
